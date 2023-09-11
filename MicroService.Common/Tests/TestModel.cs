75d943cf7b912b72981d17d763c2a56aa5f97ffa
Mukesh Adhvaryu <maadhvaryu@gmail.com>
2023-09-11 16:13:19 +0100

    CREATE class: ModelCollection<TModel, TID>
    Represents an object which holds a collection of models directly
    as in a List<TModels>.
    Implements  IModelCollection<TModel, TID>
    
    With this, we are at the stage where we can create test related entities

--- MicroService.Common/Tests/TestModel.cs
+++ MicroService.Common/Tests/TestModel.cs
@@ -3,7 +3,159 @@
 Author: Mukesh Adhvaryu.
 */
 
+//-:cnd:noEmit
+#if MODEL_ADDTEST && MODEL_USEDTO
+//+:cnd:noEmit
+
+using System.ComponentModel.DataAnnotations;
+
+using MicroService.Common.Attributes;
+using MicroService.Common.Interfaces;
+using MicroService.Common.Models;
+
 namespace MicroService.Common.Tests
 {
-    
+    #region ITestModelDTO
+    /// <summary>
+    /// Represents a DTO for test model.
+    /// </summary>
+    public interface ITestModelDTO: IModel
+    {
+        /// <summary>
+        /// Gets name of the model.
+        /// </summary>
+        string Name { get; }
+    }
+    #endregion
+
+    #region TestModelDTO
+    /// <summary>
+    /// Represents a DTO for test model.
+    /// </summary>
+    public class TestModelDTO : ITestModelDTO
+    {
+        /// <summary>
+        /// Gets name of the model.
+        /// </summary>
+        public string Name { get; }
+
+        #region CONSTRUCTOR
+        public TestModelDTO(TestModel model)
+        {
+            Name = model.Name;
+        }
+        #endregion
+    }
+    #endregion
+
+    #region TestModel
+    [Model(ProvideSeedData = true)]
+    public sealed class TestModel: Model<int>
+    {
+        #region VARIABLES
+        static int iid;
+        #endregion
+
+        #region CONSTRUCTORS
+        public TestModel(string name) :
+            base(true)
+        {
+            Name = name;
+        }
+        public TestModel() :
+            base(false)
+        { 
+
+        }
+        #endregion
+
+        #region PROPERTIES
+        [Required]  
+        public string? Name { get; set; }
+        #endregion
+
+        #region GET PROEPRTY NAMES
+        protected override IReadOnlyList<string> GetPropertyNames(bool forSearch = false) =>
+            new string[] { nameof(ID) };
+        #endregion
+
+        #region COPY FROM
+        protected override Task<bool> CopyFrom(IModel model)
+        {
+            if (!(model is TestModel))
+                return Task.FromResult(false);
+            Name = ((TestModel)model).Name;
+            return Task.FromResult(true);
+        }
+        #endregion
+
+        #region GET INITIAL DATA
+        protected override IEnumerable<IModel> GetInitialData()
+        {
+            return new TestModel[]
+            {
+               new TestModel("John"),
+               new TestModel("Timothy"),
+               new TestModel("Charlie"),
+               new TestModel("Felicity"),
+               new TestModel("Margot"),
+            };
+        }
+        #endregion
+
+        #region GET NEW ID
+        protected override int GetNewID()
+        {
+            return (++iid);
+        }
+        #endregion
+
+        #region TRY PARSE ID
+        protected override bool TryParseID(object value, out int newID) =>
+            int.TryParse(value.ToString(), out newID);
+        #endregion
+
+        #region PARSE
+        protected override Message Parse(IParameter parameter, out object currentValue, out object parsedValue, bool updateValueIfParsed = false)
+        {
+            var value = parameter is IModelParameter ? ((IModelParameter)parameter).FirstValue : parameter.Value;
+            currentValue = parsedValue = null;
+            var name = parameter.Name;
+
+            switch (parameter.Name)
+            {
+                case nameof(Name):
+                    currentValue = Name;
+                    if (value is string)
+                    {
+                        parsedValue = (string)value;
+                        return Message.Sucess(name);
+                    }
+                    if (value == null)
+                        return Message.MissingRequiredValue(name);
+                    break;
+                default:
+                    break;
+            }
+            return Message.Failure(name);
+        }
+        #endregion
+
+        //-:cnd:noEmit
+#if MODEL_USEDTO
+        #region IModelToDTO
+        protected override IModel ToDTO(Type type)
+        {
+            if (type == typeof(ITestModelDTO))
+                return new TestModelDTO(this);
+            return base.ToDTO(type);
+        }
+        #endregion
+#endif
+        //+:cnd:noEmit
+    }
+    #endregion
 }
+//-:cnd:noEmit
+#endif
+//+:cnd:noEmit
--- MicroService.Common/Tests/TestModelDTO.cs
+++ MicroService.Common/Tests/TestModelDTO.cs
@@ -1,8 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿/* Licensed under the MIT/X11 license.
+* This notice may not be removed from any source distribution.
+Author: Mukesh Adhvaryu.
+*/
+
+//-:cnd:noEmit
+#if MODEL_ADDTEST && MODEL_USEDTO
+//+:cnd:noEmit
+
+using System.ComponentModel.DataAnnotations;
+
+using MicroService.Common.Attributes;
+using MicroService.Common.Interfaces;
+using MicroService.Common.Models;
 
 namespace MicroService.Common.Tests
 {
-     
+    #region ITestModelDTO
+    /// <summary>
+    /// Represents a DTO for test model.
+    /// </summary>
+    public interface ITestModelDTO: IModel
+    {
+        /// <summary>
+        /// Gets name of the model.
+        /// </summary>
+        string Name { get; }
+    }
+    #endregion
+
+    #region TestModelDTO
+    /// <summary>
+    /// Represents a DTO for test model.
+    /// </summary>
+    public class TestModelDTO : ITestModelDTO
+    {
+        /// <summary>
+        /// Gets name of the model.
+        /// </summary>
+        public string Name { get; }
+
+        #region CONSTRUCTOR
+        public TestModelDTO(TestModel model)
+        {
+            Name = model.Name;
+        }
+        #endregion
+    }
+    #endregion
 }
+//-:cnd:noEmit
+#endif
+//+:cnd:noEmit
