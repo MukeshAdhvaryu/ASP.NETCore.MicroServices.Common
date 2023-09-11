279098a116b6f378162db571c0e00ec9895e3956
Mukesh Adhvaryu <maadhvaryu@gmail.com>
2023-09-11 19:12:25 +0100

    CREATE class: ModelCreator
    This class provides method for creating an empty model
    based on the type of controller created dynamically.
    When an interface is used in PUT or POST method,
    We need to supply concrete model so that controller can use it else the call will fail.

--- MicroService.Common/Web.API/ModelCreator.cs
+++ MicroService.Common/Web.API/ModelCreator.cs
@@ -1,10 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿/* Licensed under the MIT/X11 license.
+* This notice may not be removed from any source distribution.
+ Author: Mukesh Adhvaryu.
+*/
 
-namespace MicroService.Common.Web.API
+using System.Reflection;
+
+using MicroService.Common.Interfaces;
+using MicroService.Common.Services;
+using MicroService.Common.Web.API.Interfaces;
+
+namespace MicroService.Common.Models
 {
-    internal class ModelCreator
+    /// <summary>
+    /// This class creates model instance on calling: GetModel method.
+    /// </summary>
+    public abstract class ModelCreator
     {
+        /// <summary>
+        /// Creates model instance getiing 3rd generic parameter from specified controller type.
+        /// </summary>
+        /// <param name="controllerType">Type of controller which derives from generic Controller<,,> class</param>
+        /// <param name="modelType">Type of mmodel created.</param>
+        /// <returns>Retuns an empty instance of model.</returns>
+        /// <exception cref="NotSupportedException">Thrown when incompitible controller type or incompitent controller type is supplied.</exception>
+        protected virtual IModel GetModel(Type controllerType, out Type modelType)
+        {
+            if (!controllerType.IsAssignableTo(typeof(IExController<,,>)))
+                throw new NotSupportedException("Controller: " + controllerType.Name + " is not supported!");
+
+            var field = controllerType.GetField("service", BindingFlags.NonPublic | BindingFlags.Instance);
+            if (field == null || !field.FieldType.IsAssignableTo(typeof(IService<,,>)))
+                throw new NotSupportedException("Controller: " + controllerType.Name + " is not supported!");
+
+            var modelFieldType = field.FieldType;
+            modelType = modelFieldType.GenericTypeArguments[1];
+            var model = Activator.CreateInstance(modelType) as IExModel;
+
+            if (model == null || !(model is IExModel))
+                throw new NotSupportedException("Model: " + modelType.Name + " is not supported!");
+
+            return model;
+        }
     }
 }
