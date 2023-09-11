01b851bb877bad9fa2ce3f7f63db97467408687a
Mukesh Adhvaryu <maadhvaryu@gmail.com>
2023-09-11 12:29:10 +0100

    CREATE enums: ResultStatus, ServiceScope
    ResultStatus: to provide status of copy or update operation performed on a model.
    ServiceScope: to let user to choose the scope of service at model definition level through ModelAttrubute class.

--- MicroService.Common/Enums/Enums.cs
+++ MicroService.Common/Enums/Enums.cs
@@ -5,9 +5,58 @@
 
 namespace MicroService.Common.Models
 {
-    
+    /// <summary>
+    /// Provides status of an operation.
+    /// </summary>
+    public enum ResultStatus : byte
+    {
+        /// <summary>
+        /// Indicates that operation is failed.
+        /// </summary>
+        Failure,
+
+        /// <summary>
+        /// Indicates that operation is successful.
+        /// </summary>
+        Sucess,
+
+        /// <summary>
+        /// Indicates that operation is ignored for a specified call.
+        /// </summary>
+        Ignored,
+
+        /// <summary>
+        /// Indicates that operation  for a specified call could not be performed because of missing value.
+        /// </summary>
+        MissingValue,
+
+        /// <summary>
+        /// Indicates that operation for a specified call could not be performed because of missing value.
+        /// And, the value is required to be supplied.
+        /// </summary>
+        MissingRequiredValue,
+    }
 }
 namespace MicroService.Common.Services
 {
-   
+    /// <summary>
+    /// Provides Scope options for injecting service repository.
+    /// </summary>
+    public enum ServiceScope : byte
+    {
+        /// <summary>
+        /// Injects scoped service.
+        /// </summary>
+        Scoped,
+
+        /// <summary>
+        /// Injects transient service.
+        /// </summary>
+        Transient,
+
+        /// <summary>
+        /// Injects singleton service.
+        /// </summary>
+        Singleton,
+    }
 }
