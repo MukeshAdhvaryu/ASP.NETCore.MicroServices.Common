e52c1d5d0cf54e48aed3ad0bacb4f25a58e07a3b
Mukesh Adhvaryu <maadhvaryu@gmail.com>
2023-09-11 18:57:02 +0100

    CREATE the following interfaces: IExController<TModelDTO, TModel, TID> IExDBContext<TModel, TID>
    It is imperative that a user must use classes for Controller and
    DbContext provided herein or derived class from those base classes.
    We will check if controller and dbcontext classes are implementing our interfaces or not.

---
+++ MicroService.Common/Web.API/Interfaces.cs
@@ -0,0 +1,55 @@
+﻿using MicroService.Common.Collections;
+using MicroService.Common.Interfaces;
+using MicroService.Common.Models;
+using MicroService.Common.Services;
+
+namespace MicroService.Common.Web.API.Interfaces
+{
+    #region IExController<TModelDTO, TModel, TID>
+    /// <summary>
+    /// This interface represents a contract of operations.
+    /// </summary>
+    /// <typeparam name="TModelDTO">Interface representing the model.</typeparam>
+    /// <typeparam name="TModel">Model of your choice.</typeparam>
+    /// <typeparam name="TID">Primary key type of the model.</typeparam>
+    internal interface IExController<TModelDTO, TModel, TID> : IContract<TModelDTO, TModel, TID>
+        //+:cnd:noEmit
+        #region TYPE CONSTRINTS
+        where TModelDTO : IModel
+        where TModel : Model<TID>,
+        //-:cnd:noEmit
+#if (!MODEL_USEDTO)
+        TModelDTO,
+#endif
+        //+:cnd:noEmit
+        new()
+        where TID : struct
+        #endregion
+    {
+        IService<TModelDTO, TModel, TID> Service { get; }
+    }
+    #endregion
+
+    #region IExDBContext<TModel, TID>
+    /// <summary>
+    /// This interface represents a DbContext for entity operations.
+    /// </summary>
+    /// <typeparam name="TModel">Model of your choice.</typeparam>
+    /// <typeparam name="TID">Primary key type of the model.</typeparam>
+    internal interface IExDBContext<TModel, TID> : IModelCollection<TModel, TID>
+        //+:cnd:noEmit
+        #region TYPE CONSTRINTS
+        where TModel : Model<TID>,
+        //-:cnd:noEmit
+#if (!MODEL_USEDTO)
+        TModelDTO,
+#endif
+        //+:cnd:noEmit
+        new()
+        where TID : struct
+        #endregion
+    {
+    }
+    #endregion
+
+}
---
+++ MicroService.Common/Web.API/ModelCreator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroService.Common.Web.API
+{
+    internal class ModelCreator
+    {
+    }
+}
