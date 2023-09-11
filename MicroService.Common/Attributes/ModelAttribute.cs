061b4e714048e4ab046352a858d8e5d7322d567c
Mukesh Adhvaryu <maadhvaryu@gmail.com>
2023-09-11 12:29:10 +0100

    CRETE class: ModelAttribute
    This class provides various options for a model to represent itself through Web API.

--- MicroService.Common/Attributes/ModelAttribute.cs
+++ MicroService.Common/Attributes/ModelAttribute.cs
@@ -6,4 +6,77 @@ using MicroService.Common.Services;
 
 namespace MicroService.Common.Attributes
 {
+    #region MODEL ATTRIBUTE
+    /// <summary>
+    /// Provides various options for a model to represent itself through Web API. 
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface, AllowMultiple = false)]
+    public class ModelAttribute : Attribute
+    {
+        #region CONSTRUCTORS
+        public ModelAttribute()
+        {
+            AutoController = true;
+            ProvideSeedData = false;
+        }
+        public ModelAttribute(string name, bool autoController = true, bool provideSeedData = false)
+        {
+            Name = name;
+            AutoController = autoController;
+            ProvideSeedData = provideSeedData;
+        }
+        public ModelAttribute(string name, ServiceScope scope, bool autoController = true, bool provideSeedData = false) :
+            this(name, autoController, provideSeedData)
+        {
+            Scope = scope;
+        }
+        public ModelAttribute(ServiceScope scope, bool autoController = true, bool provideSeedData = false) :
+            this(null, scope, autoController, provideSeedData)
+        { }
+        public ModelAttribute(string name, string connectionkey, bool autoController = true, bool provideSeedData = false) :
+            this(name, autoController, provideSeedData)
+        {
+            ConnectionKey = connectionkey;
+        }
+        public ModelAttribute(string name, string connectionkey, ServiceScope scope, bool autoController = true, bool provideSeedData = false) :
+            this(name, scope, autoController, provideSeedData)
+        {
+            Scope = scope;
+            ConnectionKey = connectionkey;
+        }
+        public ModelAttribute(ServiceScope scope, string connectionkey, bool autoController = true, bool provideSeedData = false) :
+            this(null, scope, autoController, provideSeedData)
+        {
+            ConnectionKey = connectionkey;
+        }
+        #endregion
+
+        #region PROPERTIES
+        /// <summary>
+        /// Gets or sets Name of a dynamic controller to be used for a model which uses this attribute.
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets connection key to be used to connect with a db context for a model which uses this attribute.
+        /// </summary>
+        public string? ConnectionKey { get; set; }
+
+        /// <summary>
+        /// Gets or sets a scope of service to be used for a model which uses this attribute.
+        /// </summary>
+        public ServiceScope Scope { get; set; }
+
+        /// <summary>
+        /// Gets or sets a flag to choose whether to use a dynamic controller or not for a model which uses this attribute.
+        /// </summary>
+        public bool AutoController { get; set; }
+
+        /// <summary>
+        /// Gets or sets a flag to indicate whether or not to provide an initial collection of models when model collection is empty for a model which uses this attribute.
+        /// </summary>
+        public bool ProvideSeedData { get; set; }
+        #endregion
+    }
+    #endregion
 }
--- MicroService.Common/Interfaces/Abilities.cs
+++ MicroService.Common/Interfaces/Abilities.cs
@@ -6,5 +6,68 @@ using MicroService.Common.Models;
 
 namespace MicroService.Common.Interfaces
 {
-   
+    #region IModelCount
+    /// <summary>
+    /// Represents an object which offers a number of models it holds.
+    /// </summary>
+    public interface IModelCount
+    {
+        /// <summary>
+        /// Gets the number of models this object currently holds.
+        /// </summary>
+        /// <returns></returns>
+        int GetModelCount();
+    }
+    #endregion
+
+    #region IUpdatable<T>
+    /// <summary>
+    /// This object represents a model which can be updated from the given value store.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IUpdatable<T>
+    {
+        /// <summary>
+        /// Handles value copy operation of a given property.
+        /// </summary>
+        /// <param name="store">Value store to copy value/s from.</param>
+        /// <param name="notification">Out notification to indicate status of copy operation.</param>
+        /// <param name="message">Out message to indicate success/failure of copy operation status.</param>
+        void Update(IValueStore<T> store, out BindingResultStatus notification, out string message);
+    }
+    #endregion
+
+    #region IMatch
+    /// <summary>
+    /// Reprents an object which checks if certqain value exists.
+    /// </summary>
+    public interface IMatch
+    {
+        /// <summary>
+        /// Finds whether the given value matches the current value of property found by specified property name.
+        /// </summary>
+        /// <param name="propertyName">NAme of property to search for.</param>
+        /// <param name="value">Value to find if there is a match or not.</param>
+        /// <returns>True if values match, otherwise false.</returns>
+        bool IsMatch(string propertyName, object value);
+    }
+    #endregion
+
+    #region IModifiable
+    //-:cnd:noEmit
+#if MODEL_APPENDABLE || MODEL_UPDATABLE || MODEL_DELETABLE
+    /// <summary>
+    /// This interface represents an object that is aware of changes in its internal state and allows saving those changes.
+    /// </summary>
+    public interface IModifiable
+    {
+        /// <summary>
+        /// Saves changes made to this object.
+        /// </summary>
+        /// <returns>True if the operation is successful; otherwise, false.</returns>
+        Task<bool> SaveChanges();
+    }
+#endif
+    //+:cnd:noEmit
+    #endregion
 }
