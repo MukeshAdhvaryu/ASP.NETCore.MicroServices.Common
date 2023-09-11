2196bba320d124b590446d44ad09c927c38453f4
Mukesh Adhvaryu <maadhvaryu@gmail.com>
2023-09-11 19:51:32 +0100

    CREATE the follwing classes: ModelBinder : Implements IModelBinder,  uses incoming request(POST, PUT) to create and update a concrete model with the values extracted and send it back to controller to use further.
    ParamBinder: Implements IModelBinder, uses incoming request(GET)
    to create and update a concrete search parameter/s with the values
    extracted and send them back to controller to use further.

---
+++ MicroService.Common/Web.API/ModelBinder.cs
@@ -0,0 +1,82 @@
+﻿/* Licensed under the MIT/X11 license.
+* This notice may not be removed from any source distribution.
+ Author: Mukesh Adhvaryu.
+*/
+
+//-:cnd:noEmit
+#if !TDD
+//+:cnd:noEmit
+using System.Net;
+
+using MicroService.Common.Constants;
+using MicroService.Common.Models;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace MicroService.Common.Web.API
+{
+    public sealed class ModelBinder : ModelCreator, IModelBinder
+    {
+        #region BIND MODEL
+        async Task IModelBinder.BindModelAsync(ModelBindingContext bindingContext)
+        {
+            var descriptor = (ControllerActionDescriptor)bindingContext.ActionContext.ActionDescriptor;
+            var model = (IExModel)GetModel(descriptor.ControllerTypeInfo, out Type modelType);
+
+            bool NeedToUseDTO = !bindingContext.ModelType.IsAssignableFrom(modelType);
+
+            var Query = bindingContext.ActionContext.HttpContext.Request.Query;
+
+            List<Message> messages = new List<Message>();
+            var PropertyNames = model.GetPropertyNames();
+            bool failed = false;
+
+            foreach ( var name in PropertyNames )
+            {
+                var parameter = Query.ContainsKey(name) ? new ModelParameter(Query[name], name) : new ModelParameter(name);
+                var message = model.Parse(parameter, out _, out _, true);
+
+                switch (message.Status)
+                {
+                    case ResultStatus.Failure:
+                    case ResultStatus.MissingRequiredValue:
+                        failed = true;
+                        break;
+                    default:
+                        break;
+                }
+                messages.Add(message);
+            }            
+
+            if (!failed)
+            {
+                //-:cnd:noEmit
+#if MODEL_USEDTO
+                if (NeedToUseDTO)
+                    bindingContext.Result = ModelBindingResult.Success(model.ToDTO(bindingContext.ModelType));
+                else
+                    bindingContext.Result = ModelBindingResult.Success(model);
+
+#else
+                bindingContext.Result = ModelBindingResult.Success(model);
+
+#endif
+                //+:cnd:noEmit
+                return;
+            }
+
+            var response = bindingContext.HttpContext.Response;
+            response.StatusCode = (int)HttpStatusCode.ExpectationFailed;
+            response.ContentType = Contents.JSON;
+            await response.WriteAsJsonAsync(messages.Select((m, i) => ++i+ ". " + m));
+            await response.CompleteAsync();
+            return;
+        }
+        #endregion
+    }
+}
+//-:cnd:noEmit
+#endif
+//+:cnd:noEmit
---
+++ MicroService.Common/Web.API/ParamBinder.cs
@@ -0,0 +1,66 @@
+﻿/* Licensed under the MIT/X11 license.
+* This notice may not be removed from any source distribution.
+ Author: Mukesh Adhvaryu.
+*/
+
+#if !TDD
+using System.Reflection.Metadata;
+
+using MicroService.Common.Models;
+using MicroService.Common.Parameters;
+
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace MicroService.Common.Web.API
+{
+    public sealed class ParamBinder: ModelCreator, IModelBinder
+    {
+        async Task IModelBinder.BindModelAsync(ModelBindingContext bindingContext)
+        {
+            var descriptor = (ControllerActionDescriptor)bindingContext.ActionContext.ActionDescriptor;
+            var model = (IExModel)GetModel(descriptor.ControllerTypeInfo, out _);
+
+            var Query = bindingContext.ActionContext.HttpContext.Request.Query;
+            bool multiple = Query.ContainsKey(bindingContext.OriginalModelName);
+
+            List<SearchParameter> list = new List<SearchParameter>();
+            var PropertyNames = model.GetPropertyNames();
+
+            if (multiple)
+            {
+                var items = Query[bindingContext.OriginalModelName];
+                foreach (var item in items)
+                {
+                    //working on it. in next version....
+                }
+
+            }
+            else
+            {
+                foreach (var name in PropertyNames)
+                {
+                    if (Query["name"][0].ToLower() == name.ToLower())
+                    {
+                        var parameter = new ModelParameter(Query["value"], name);
+                        var message = model.Parse(parameter, out _, out object value, false);
+                        if (message.Status == ResultStatus.Sucess)
+                        {
+                            Enum.TryParse(Query["criteria"], out Criteria criteria);
+                            Enum.TryParse(Query["andor"], out AndOr andOr);
+
+                            list.Add(new SearchParameter(name, value, criteria, andOr));
+                        }
+                    }
+                }
+            }
+            if(multiple)
+            {
+                bindingContext.Result = ModelBindingResult.Success(list);
+                return;
+            }
+            bindingContext.Result = ModelBindingResult.Success(list.FirstOrDefault());
+        }
+    }
+}
+#endif
