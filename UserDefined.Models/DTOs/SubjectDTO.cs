04d9d46689ae5f6d86d307c54ba8f2ad11fadffc
Mukesh Adhvaryu <maadhvaryu@gmail.com>
2023-09-11 22:30:12 +0100

    CREATE proj (shared): UserDefined Define your own models here. In order to test your own models add: TEST_USERMODELS condition constant to your test projects and follw the comments written in test classes. Add one test file per user defined model.

---
+++ UserDefined.Models/DTOs/SubjectDTO.cs
@@ -0,0 +1,42 @@
+﻿//-:cnd:noEmit
+#if MODEL_USEDTO
+//+:cnd:noEmit
+using System.ComponentModel.DataAnnotations;
+
+using MicroService.Common.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+using UserDefined.Models;
+
+using JsonConverter = System.Text.Json.Serialization.JsonConverter;
+
+namespace UserDefined.DTOs
+{
+    //[Model(Scope = MicroService.Common.Services.ServiceScope.Singleton)]
+    public interface ISubjectDTO : IModel
+    {
+        [Required]
+        string? Name { get; }
+
+        [Required]
+        [JsonConverter(typeof(StringEnumConverter))]
+        Faculty Faculty { get; }
+    }
+
+    //[Model(Scope = MicroService.Common.Services.ServiceScope.Singleton)]
+    public class SubjectDTO : ISubjectDTO
+    {
+        public SubjectDTO(ISubject subject)
+        {
+            Name = subject.Name;
+            Faculty = subject.Faculty;
+        }
+        public string? Name { get; }
+        public Faculty Faculty { get; }
+    }
+}
+
+//-:cnd:noEmit
+#endif
+//+:cnd:noEmit
