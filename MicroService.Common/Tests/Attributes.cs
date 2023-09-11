5f62753bb18dd9caf70c99912b8f7813fe31bc79
Mukesh Adhvaryu <maadhvaryu@gmail.com>
2023-09-11 17:12:43 +0100

    CREATE classes: Common test attributes.
    Depending on which testing framework is used, base classes of these
    classes are changed to inherit an appropriate framework support.
    
    TestableAttribute: Top class level attrubute to mark a class for testing.
    
    NoArgsAttribute: Method level attribute suggesting the method without
    having any parameters.
    
    WithArgsAttribute: Method level attribute suggesting the method is
    having one or more parameters.

--- MicroService.Common/Tests/Attributes.cs
+++ MicroService.Common/Tests/Attributes.cs
@@ -2,8 +2,101 @@
 * This notice may not be removed from any source distribution.
  Author: Mukesh Adhvaryu.
 */
+//-:cnd:noEmit
+#if MODEL_ADDTEST
+//+:cnd:noEmit
+
+using System.Reflection;
+
+
+//-:cnd:noEmit
+#if MODEL_USEXUNIT
+using Xunit;
+using Xunit.Sdk;
+#elif MODEL_USENUNIT
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+#else
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+#endif
+//+:cnd:noEmit
 
 namespace MicroService.Common.Tests.Attributes
 {
+    //-:cnd:noEmit
+#if MODEL_USEXUNIT
+    public class TestableAttribute : Attribute { }
+    public class NoArgsAttribute : FactAttribute { }
+    public class WithArgsAttribute : TheoryAttribute { }
+
+    [DataDiscoverer("Xunit.Sdk.InlineDataDiscoverer", "xunit.core")]
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class ArgsAttribute : DataAttribute
+    {
+        readonly object[] data;
+
+        public ArgsAttribute(params object[] _data)
+        {
+            data = _data;
+        }
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            return new[] { data };
+        }
+    }
+
+
+#elif MODEL_USENUNIT
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
+    public class TestableAttribute : TestFixtureAttribute { }
+
+    public class NoArgsAttribute : TestAttribute { }
+    public class WithArgsAttribute : TestAttribute { }
+
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = false)]
+    public class ArgsAttribute : TestCaseAttribute
+    {
+        public ArgsAttribute(params object?[]? arguments) :
+            base(arguments)
+        { }
+
+        public ArgsAttribute(object? arg) :
+            base(arg)
+        { }
+
+        public ArgsAttribute(object? arg1, object? arg2) :
+            base(arg1, arg2)
+        { }
+
+        public ArgsAttribute(object? arg1, object? arg2, object? arg3) :
+            base(arg1, arg2, arg3)
+        { }
+    }
+
+#else
+    public class TestableAttribute : TestClassAttribute { }
+    public class NoArgsAttribute : TestMethodAttribute { }
+    public class WithArgsAttribute : TestMethodAttribute { }
+
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class ArgsAttribute : DataRowAttribute
+    {
+        public ArgsAttribute() :
+            base()
+        { }
+
+        public ArgsAttribute(object data1) :
+            base(data1)
+        { }
+
+        public ArgsAttribute(object data1, params object[] moreData) :
+            base(data1, moreData)
+        { }
+    }
+#endif
+    //+:cnd:noEmit
 }
+//-:cnd:noEmit
+#endif
+//+:cnd:noEmit
 
