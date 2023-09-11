1edeb8a5b6859ccc6ca69c371d8d704d499e15ac
Mukesh Adhvaryu <maadhvaryu@gmail.com>
2023-09-11 14:20:07 +0100

    CREATE class: Model- base class for all models.
    Implements     public interface IModel, IModel<TIDType>, IExModel, IExModel<TIDType>, IExCopyable, IUpdatable<string>, IMatch

--- MicroService.Common/Models/Model.cs
+++ MicroService.Common/Models/Model.cs
@@ -72,12 +72,6 @@ namespace MicroService.Common.Models
         /// </summary>
         /// <returns>Newly generated id.</returns>
         TIDType GetNewID();
-
-        /// <summary>
-        /// Gets ID of a model created last.
-        /// </summary>
-        /// <returns></returns>
-        TIDType GetLastGeneratedID();
     }
     #endregion
 
@@ -111,6 +105,169 @@ namespace MicroService.Common.Models
         /// <param name="configuration"></param>
         /// <returns></returns>
         Action<TOptionBuilder> GetOptionsBuilderAction<TOptionBuilder>(params IParameter[] parameters);
+    }
+    #endregion
+
+    #region Model<TIDType>
+    /// <summary>
+    /// This class represents a base class for any user-defined model.
+    /// Highly customizable by using the following conditional compilation symbols:
+    /// MODEL_DELETABLE;
+    /// MODEL_APPENDABLE;
+    /// MODEL_UPDATABLE;
+    /// MODEL_USEMYOWNCONTROLLER
+    /// </summary>
+    public abstract partial class Model<TIDType> : IExModel<TIDType>, IExModel
+        where TIDType : struct
+    {
+        #region VARIABLES
+        protected TIDType id;
+        #endregion
+
+        #region CONSTRUCTOR
+        protected Model(bool generateNewID)
+        {
+            if (generateNewID)
+                id = GetNewID();
+        }
+        #endregion
+
+        #region PROPERTIES
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public TIDType ID { get => id; set => id = value; }
+        TIDType IExModel<TIDType>.ID { get => id; set => id = value; }
+
+        /// <summary>
+        /// Provides a list of names of properties - must be handled while copying from data supplied from model binder's BindModelAsync method.
+        /// If the list is not provided, System.Reflecteion will be used to obtain names of the properties defined in this model.
+        /// </summary>
+        protected virtual IReadOnlyList<string> Properties => null;
+
+        IReadOnlyList<string> IExModel.Properties
+        {
+            get
+            {
+                var propertyNames = this.Properties;
+
+                if (propertyNames == null || propertyNames.Count == 0)
+                {
+                    propertyNames = GetType().GetProperties().Where(p =>
+                    {
+                        var attr = p.GetType().GetCustomAttribute<DatabaseGeneratedAttribute>();
+                        if (attr?.DatabaseGeneratedOption == DatabaseGeneratedOption.Computed)
+                            return false;
+                        return true;
+                    }).Select(p => p.Name).ToArray();
+                }
+                return propertyNames;
+            }
+        }
+        #endregion
+
+        #region UPDATE
+        /// <summary>
+        /// Handles value copy operation of a given property.
+        /// </summary>
+        /// <param name="store">Value store to copy value/s from.</param>
+        /// <param name="notification">Out notification to indicate status of copy operation.</param>
+        /// <param name="message">Out message to indicate success/failure of copy operation status.</param>
+        protected abstract void Update(IValueStore<string> value, out BindingResultStatus notification, out string message);
+
+        void IUpdatable<string>.Update(IValueStore<string> store, out BindingResultStatus notification, out string message) =>
+            Update(store, out notification, out message);
+        #endregion
+
+        #region COPY FROM
+        /// <summary>
+        /// Copies data from the given model to this instance.
+        /// </summary>
+        /// <param name="model">Model to copy data from.</param>
+        /// <returns></returns>
+        protected abstract Task<bool> CopyFrom(IModel model);
+
+        Task<bool> IExCopyable.CopyFrom(IModel model)
+        {
+            if (model is IModel<TIDType>)
+            {
+                var m = (IModel<TIDType>)model;
+                id = m.ID;
+                return CopyFrom(model);
+            }
+
+            //-:cnd:noEmit
+#if MODEL_USEDTO
+            if (model is IModel)
+            {
+                if (Equals(id, default(TIDType)))
+                    id = GetNewID();
+                return CopyFrom(model);
+            }
+#endif
+            //+:cnd:noEmit
+            return Task.FromResult(false);
+        }
+        #endregion
+
+        #region GET INITIAL DATA
+        /// <summary>
+        /// Gets initial data.
+        /// </summary>
+        /// <returns>IEnumerable\<IModel\> containing list of initial data.</returns>
+        protected abstract IEnumerable<IModel> GetInitialData();
+
+        IEnumerable<IModel> IExModel.GetInitialData() => GetInitialData();
+        #endregion
+
+        #region GET OPTION BUILDER ACTION
+        protected abstract Action<TOptionBuilder> GetOptionsBuilderAction<TOptionBuilder>(params IParameter[] parameters);
+        Action<TOptionBuilder> IExModel.GetOptionsBuilderAction<TOptionBuilder>(params IParameter[] parameters) =>
+            GetOptionsBuilderAction<TOptionBuilder>(parameters);
+        #endregion
+
+        #region GET NEW ID
+        protected abstract TIDType GetNewID();
+        TIDType IExModel<TIDType>.GetNewID()
+        {
+            return GetNewID();
+        }
+        #endregion
+
+        #region MATCH
+        protected abstract bool Match(string propertyName, object value);
+        bool IMatch.IsMatch(string propertyName, object value)
+        {
+            if (propertyName == nameof(ID))
+            {
+                if (value is TIDType)
+                    return Equals(id, value);
+                return false;
+            }
+            return Match(propertyName, value);
+        }
+        #endregion
+
+        //-:cnd:noEmit
+#if MODEL_USEDTO
+        #region IModelToDTO
+        /// <summary>
+        /// Provides compitible DTO of given type from this model.
+        /// You must override this method to support dtos.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns>Compitible DTO.</returns>
+        protected virtual IModel ToDTO(Type type)
+        {
+            var t = GetType();
+            if(type == t || t.IsAssignableTo(type))
+                return this;
+            return null;
+        }
+        IModel IExModelToDTO.ToDTO(Type type) =>
+            ToDTO(type);
+        #endregion
+#endif
+        //+:cnd:noEmit
     }
     #endregion
 }
