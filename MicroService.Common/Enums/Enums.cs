af542308d1f4b8234152f40a94ab7a386b7f8f44
Mukesh Adhvaryu <maadhvaryu@gmail.com>
2023-09-11 12:13:19 +0100

    CREATE enums: BindingResultStatus, ServiceScope
    BindingResultStatus: to provide status of copy or update operation performed on a model.
    ServiceScope: to let user to choose the scope of service at model definition level through ModelAttrubute class.

--- MicroService.Common/Enums/Enums.cs
+++ MicroService.Common/Enums/Enums.cs
@@ -5,5 +5,58 @@
 
 namespace MicroService.Common.Models
 {
-    
+    /// <summary>
+    /// Provides binding status while copy data as a part of model binding.
+    /// </summary>
+    public enum BindingResultStatus : byte
+    {
+        /// <summary>
+        /// Indicates that binding operation is successful.
+        /// </summary>
+        Sucess,
+
+        /// <summary>
+        /// Indicates that binding operation is failed.
+        /// </summary>
+        Failure,
+
+        /// <summary>
+        /// Indicates that binding operation is ignored for a specified call.
+        /// </summary>
+        Ignored,
+
+        /// <summary>
+        /// Indicates that binding operation  for a specified call could not be performed because of missing value.
+        /// </summary>
+        MissingValue,
+
+        /// <summary>
+        /// Indicates that binding operation  for a specified call could not be performed because of missing value.
+        /// However, the value is required to be supplied.
+        /// </summary>
+        MissingRequiredValue,
+    }
+}
+namespace MicroService.Common.Services
+{
+    /// <summary>
+    /// Provides Scope options for injecting service repository.
+    /// </summary>
+    public enum ServiceScope : byte
+    {
+        /// <summary>
+        /// Injects scoped service.
+        /// </summary>
+        Scoped,
+
+        /// <summary>
+        /// Injects transient service.
+        /// </summary>
+        Transient,
+
+        /// <summary>
+        /// Injects singleton service.
+        /// </summary>
+        Singleton,
+    }
 }
