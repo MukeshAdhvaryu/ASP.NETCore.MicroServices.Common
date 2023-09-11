30085f7d19297c9ce5c82fdfd63891a1bef57a2f
Mukesh Adhvaryu <maadhvaryu@gmail.com>
2023-09-11 16:51:22 +0100

    CREATE class: ModelCollection<TModel, TIDType>
    Represents an object which holds a collection of models directly as in a List<TModels>.
    
    Implements  IModelCollection<TModel, TIDType>

--- MicroService.Common/ModelCollection/ModelCollection.cs
+++ MicroService.Common/ModelCollection/ModelCollection.cs
@@ -124,4 +124,254 @@ namespace MicroService.Common.Interfaces
         #endregion
     }
     #endregion
+
+    #region ModelCollection<TModel, TIDType>
+    /// <summary>
+    /// Represents an object which holds a collection of models directly or indirectly.
+    /// </summary>
+    /// <typeparam name="TModel">Type of Model<typeparamref name="TIDType"/></typeparam>
+    /// <typeparam name="TIDType">Type of TIDType</typeparam>
+    public class ModelCollection<TModel, TIDType> : IModelCollection<TModel, TIDType>
+    //-:cnd:noEmit
+#if !MODEL_NONREADABLE
+    , IEnumerable<TModel>
+#endif
+#if MODEL_APPENDABLE || MODEL_UPDATABLE || MODEL_DELETABLE
+        , IModifiable
+#endif
+    //+:cnd:noEmit
+        #region TYPE CONSTRAINTS
+    where TModel : Model<TIDType>, new()
+    where TIDType : struct
+        #endregion
+    {
+        #region VARIABLES
+        List<TModel> models = new List<TModel>(5);
+        #endregion
+
+        #region CONSTRUCTORS
+        public ModelCollection()
+        {
+            bool provideSeedData = false;
+            var attribute = typeof(TModel).GetCustomAttribute<ModelAttribute>();
+            if (attribute != null)
+                provideSeedData = attribute.ProvideSeedData;
+            if (provideSeedData && !models.Any())
+            {
+                var model = (IExModel)new TModel();
+                var items = model.GetInitialData();
+                if (items != null)
+                {
+                    models.AddRange(items.OfType<TModel>());
+                }
+            }
+        }
+        #endregion
+
+        #region FIND
+        //-:cnd:noEmit
+#if !MODEL_NONREADABLE
+        /// <summary>
+        /// Finds a model based on given keys.
+        /// </summary>
+        /// <param name="keys">Keys to be used to find the model.</param>
+        /// <returns>Task with result of type TModel.</returns>
+        public Task<TModel?> Find(IEnumerable<IParameter> keys)
+        {
+            Predicate<TModel> predicate = (m) =>
+            {
+                IMatch match = m;
+
+                foreach (var key in keys)
+                {
+                    if (key == null)
+                        continue;
+                    if (!match.IsMatch(key.Name, key.Value))
+                        return false;
+                }
+                return true;
+            };
+            return Task.FromResult(models.FirstOrDefault((m) => predicate(m)));
+        }
+
+        /// <summary>
+        /// Finds all models matched based on given key.
+        /// </summary>
+        /// <param name="key">Keys to be used to find the model.</param>
+        /// <returns>Task with result of type TModel.</returns>
+        public Task<IEnumerable<TModel>> FindAll(IParameter key)
+        {
+            Func<TModel, bool> func = (m) =>
+            {
+                return ((IMatch)m).IsMatch(key.Name, key.Value);
+            };
+            return Task.FromResult(models.Where(m => func(m)));
+        }
+
+        /// <summary>
+        /// Finds a model based on given keys.
+        /// </summary>
+        /// <param name="keys">Keys to be used to find the model.</param>
+        /// <returns>Task with result of type TModel.</returns>
+        public Task<TModel?> Find(TIDType id)
+        {
+            return Task.FromResult(models.FirstOrDefault(m => Equals(m.ID, id)));
+        }
+#endif
+        //+:cnd:noEmit
+        #endregion
+
+        #region ADD
+        //-:cnd:noEmit
+#if MODEL_APPENDABLE
+        /// <summary>
+        /// Adds a specified model.
+        /// </summary>
+        /// <param name="model">Model to add.</param>
+        /// <returns>Task with result of type boolean.</returns>
+        public Task<bool> Add(TModel model)
+        {
+            if (models.Any(m => Equals(m.ID, model.ID)))
+                return Task.FromResult(false);
+
+            if (Equals(model.ID, default(TIDType)))
+            {
+                var exModel = (IExModel<TIDType>)model;
+                exModel.ID = exModel.GetNewID();
+            }
+
+            models.Add(model);
+            return Task.FromResult(true);
+        }
+
+        /// <summary>
+        /// Adds a range of specified models.
+        /// </summary>
+        /// <param name="models">Models to add.</param>
+        /// <returns>Task with result of type boolean.</returns>
+        public Task<bool> AddRange(IEnumerable<TModel> models)
+        {
+            var list = new List<TModel>();
+
+            foreach(var model in models)
+            {
+                if (model == null)
+                    continue;
+                if (models.Any(m => Equals(m.ID, model.ID)))
+                    continue;
+                if (Equals(model.ID, default(TIDType)))
+                {
+                    var exModel = (IExModel<TIDType>)model;
+                    exModel.ID = exModel.GetNewID();
+                }
+                list.Add(model);
+            }
+            this.models.AddRange(list);
+            return Task.FromResult(list.Count > 0);
+        }
+#endif
+        //+:cnd:noEmit
+        #endregion
+
+        #region DELTE
+        //-:cnd:noEmit
+#if MODEL_DELETABLE
+        /// <summary>
+        /// Adds a specified model.
+        /// </summary>
+        /// <param name="model">Model to delete.</param>
+        /// <returns>Task with result of type boolean.</returns>
+        public Task<bool> Delete(TModel model)
+        {
+            return Task.FromResult(models.Remove(model));
+        }
+
+        /// <summary>
+        /// Deletes a range of specified models.
+        /// </summary>
+        /// <param name="models">Models to delete.</param>
+        /// <returns>Task with result of type boolean.</returns>
+        public Task<bool> DeleteRange(IEnumerable<TModel> models)
+        {
+            var list = this.models.Except(models).ToList();
+            var changed = list.Count != this.models.Count;
+            this.models = list;
+            return Task.FromResult(changed);
+        }
+#endif
+        //+:cnd:noEmit
+        #endregion
+
+        #region UPDATE
+        //-:cnd:noEmit
+#if MODEL_UPDATABLE
+        /// <summary>
+        /// Updates a specified model.
+        /// </summary>
+        /// <param name="model">Model to update.</param>
+        /// <returns>Task with result of type boolean.</returns>
+        public async Task<bool> Update(TModel model)
+        {
+            var result = models.FirstOrDefault(m => Equals(m.ID, model.ID));
+            if(result == default(TModel))
+                return false;
+            return await ((IExModel)result).CopyFrom(model);
+        }
+
+        /// <summary>
+        /// Updates a range of specified models.
+        /// </summary>
+        /// <param name="models">Models to update.</param>
+        /// <returns>Task with result of type boolean.</returns>
+        public async Task<bool> UpdateRange(IEnumerable<TModel> models)
+        {
+            var results = models.Select((ext) => Tuple.Create(ext, this.models.FirstOrDefault(m => Equals(m.ID, ext.ID))));
+            bool result = false;
+
+            foreach (var tuple in results)
+            {
+                if (tuple.Item2 == default(TModel))
+                    continue;
+                var rslt = await ((IExModel)tuple.Item2).CopyFrom(tuple.Item1);
+                if (rslt)
+                    result = true;
+            }
+            return result;
+        }
+#endif
+        //+:cnd:noEmit
+        #endregion
+
+        #region SAVE CHANGES
+        //-:cnd:noEmit
+#if MODEL_APPENDABLE || MODEL_UPDATABLE || MODEL_DELETABLE
+        Task<bool> IModifiable.SaveChanges() =>
+            Task.FromResult(true);
+#endif
+        //+:cnd:noEmit
+        #endregion
+
+        #region GET FIRST MODEL
+        TModel? IFirstModel<TModel, TIDType>.GetFirstModel() =>
+            models.FirstOrDefault();
+        #endregion
+
+        #region GET MODEL COUNT
+        int IModelCount.GetModelCount() =>
+            models.Count();
+        #endregion
+
+        #region ENUMERATORS
+        //-:cnd:noEmit
+#if !MODEL_NONREADABLE
+        public IEnumerator<TModel> GetEnumerator() =>
+            models.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() =>
+            ((IEnumerable<TModel>)this).GetEnumerator();
+#endif
+        //+:cnd:noEmit
+        #endregion
+    }
+
+    #endregion
 }
