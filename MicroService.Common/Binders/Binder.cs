8a4fb726bcb5076927d19b63a03cf54f373ae162
Mukesh Adhvaryu <maadhvaryu@gmail.com>
2023-09-12 09:50:06 +0100

    CREATE Binder<T> class CREATE BindingMessageResult struct
    To provide an abstract base class to be inherited by model binder later.

---
+++ MicroService.Common/Binders/Binder.cs
@@ -0,0 +1,48 @@
+﻿/* Licensed under the MIT/X11 license.
+* This notice may not be removed from any source distribution.
+ Author: Mukesh Adhvaryu.
+*/
+
+using MicroService.Common.Interfaces;
+
+namespace MicroService.Common.Models
+{
+    /// <summary>
+    /// Provides Update method to update a model class.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class Binder<T> 
+    {
+        #region BIND MODEL
+        protected Task<Tuple<bool, BindingResultMessage[]>>  Update<U>(IUpdatable<T> model, IReadOnlyList<U> valueList)
+            where U: IValueStore<T>
+        {
+            var bindingResults = new BindingResultMessage[valueList.Count];
+            int i = -1;
+            string message;
+            BindingResultStatus notification;
+            bool failed = false;
+
+            if (valueList.Count > 0)
+            {
+                foreach (var result in valueList)
+                {
+                    model.Update(result, out notification, out message);
+                    bindingResults[++i] = new BindingResultMessage(message, notification);
+
+                    switch (bindingResults[i].Notification)
+                    {
+                        case BindingResultStatus.Failure:
+                        case BindingResultStatus.MissingRequiredValue:
+                            failed = true;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            return Task.FromResult(Tuple.Create(!failed, bindingResults));
+        }
+        #endregion
+    }
+}
---
+++ MicroService.Common/Binders/BindingResultMessage.cs
@@ -0,0 +1,29 @@
+﻿/* Licensed under the MIT/X11 license.
+* This notice may not be removed from any source distribution.
+ Author: Mukesh Adhvaryu.
+*/
+
+namespace MicroService.Common.Models
+{
+    #region BINDING RESULT MESSAGE STRUCT
+    public struct BindingResultMessage
+    {
+        public readonly string Message;
+
+        //[JsonConverter(typeof(StringEnumConverter))]
+        public readonly BindingResultStatus Notification;
+
+        public BindingResultMessage(string message, BindingResultStatus notification = 0)
+        {
+            Message = message;
+            Notification = notification;
+        }
+
+        public override string ToString()
+        {
+            return Notification + " " + Message;
+        }
+    }
+    #endregion
+
+}
