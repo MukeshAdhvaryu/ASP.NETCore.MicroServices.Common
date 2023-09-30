585a8d60030a1dc1de7e858d1d714a5d5b1b4b89
Mukesh Adhvaryu <maadhvaryu@gmail.com>
2023-09-30 13:40:23 +0100

    BEGIN ADD: Support for IActionResult in Controller.

---
+++ MicroService.Common/Web.API/Interfaces/Abilities.cs
@@ -0,0 +1,19 @@
+﻿/* Licensed under the MIT/X11 license.
+* This notice may not be removed from any source distribution.
+ Author: Mukesh Adhvaryu.
+*/
+//-:cnd:noEmit
+#if !TDD && MODEL_USEACTION
+//+:cnd:noEmit
+
+using MicroService.Common.Models;
+using MicroService.Common.Parameters;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroService.Common.Web.API.Interfaces
+{
+}
+//-:cnd:noEmit
+#endif
+//+:cnd:noEmit
---
+++ MicroService.Common/Web.API/Interfaces/IActionContract.cs
@@ -0,0 +1,18 @@
+﻿/* Licensed under the MIT/X11 license.
+* This notice may not be removed from any source distribution.
+ Author: Mukesh Adhvaryu.
+*/
+//-:cnd:noEmit
+#if !TDD && MODEL_USEACTION
+//+:cnd:noEmit
+
+using MicroService.Common.Interfaces;
+using MicroService.Common.Models;
+
+namespace MicroService.Common.Web.API.Interfaces
+{
+    
+}
+//-:cnd:noEmit
+#endif
+//+:cnd:noEmit
